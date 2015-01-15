9cbbf2a18b4c87bd4e836900a6451d036776aee6
Kate Hedgpeth <katehedgpeth@gmail.com>
2015-01-15 17:10:40 -0500

    testing out webhooks

---
+++ app/controllers/payload_controller.rb
@@ -0,0 +1,8 @@
+class PayloadController < ApplicationController
+  def index
+	post '/payload' do
+	  push = JSON.parse(request.body.read)
+	  puts "I got some JSON: #{push.inspect}"
+	end
+  end
+end
